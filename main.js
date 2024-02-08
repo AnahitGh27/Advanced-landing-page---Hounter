21806478988dbbf738024f737c4ba8f17dfaf600
anghazaryan <anghazaryan@griddynamics.com>
2024-02-08 11:36:54 +0400

    add new styles for custom-select

--- main.js
+++ main.js
@@ -12,4 +12,5 @@ slider();
 addArticles();
 chnageArticle();
 closeCookie();
+document.querySelector('body').style.visibility = '';
 
--- src/toggleSelect.js
+++ src/toggleSelect.js
@@ -3,6 +3,7 @@ function toggleSelect() {
     const arrowIcon = document.querySelector('.custom-select__btn--icon');
     const formSelect = document.querySelector('.form__select');
     optionsContainer.classList.toggle('custom-select__options-container--active');
+
     if (optionsContainer.classList.contains('custom-select__options-container--active')) {
         arrowIcon.src = 'img/icons/arrow_up.svg';
         formSelect.style.marginBottom = '220px';
@@ -10,11 +11,12 @@ function toggleSelect() {
         arrowIcon.src = 'img/icons/arrow_down.svg';
         formSelect.style.marginBottom = '0px';
     }
-    
 }
   
 function selectOption(option) {
-    document.getElementById('selectedOption').textContent = option;
+    const selectedOption = document.getElementById('selectedOption');
+    selectedOption.textContent = option;
+    selectedOption.style.color = '#1B1C57';
     toggleSelect();
 }
   
@@ -25,7 +27,19 @@ export  function customSelect() {
     const options = document.querySelectorAll('.custom-select__option');
     options.forEach((option) => {
         option.addEventListener('click', () => {
-        selectOption(option.textContent);
+            selectOption(option.textContent);
         });
+    });
+
+    document.addEventListener('click', (event) => {
+        const target = event.target;
+        const isCustomSelect = target.closest('.custom-select');
+
+        if (!isCustomSelect) {
+            const optionsContainer = document.querySelector('.custom-select__options-container');
+            optionsContainer.classList.remove('custom-select__options-container--active');
+            const arrowIcon = document.querySelector('.custom-select__btn--icon');
+            arrowIcon.src = 'img/icons/arrow_down.svg';
+        }
     });
 }
